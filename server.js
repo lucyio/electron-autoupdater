a3b77b3a343b8294a26d15761456e0be4e87d6d0
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2018-08-28 16:15:00 +0200

    set up repo

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const http = require('http')
 const hazel = require('hazel-server')
 
 require('dotenv').config()
-console.log(process.env)
 const PORT = process.env.PORT || 3000
 
-http.createServer((req, res) => {
-  hazel({
-    account: process.env.ACCOUNT,
-    repository: process.env.REPOSITORY
-  })
-}).listen(PORT, () => {
-  console.log(`Electron autoupdater booted on localhost:${PORT}`)
+const server = hazel({
+  account: process.env.ACCOUNT,
+  repository: process.env.REPOSITORY
 })
+
+http
+  .createServer((req, res) => server(req, res) )
+  .listen(PORT, () => {
+    console.log(`Electron autoupdater booted on localhost:${PORT}`)
+  })
